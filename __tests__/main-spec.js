d5079bd07dc791e4bc8d26a3b38b94def7044de3
Eddie Fong <fonged@oocl.com>
2018-11-22 18:00:56 +0800

    code refactor

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -2,54 +2,54 @@ const printReceipt = require("../main");
 
 it ("constItemDict", () => {
 	let input = [
-		'ITEM000001',
-		'ITEM000001',
-		'ITEM000001',
-		'ITEM000001',
-		'ITEM000001',
-		'ITEM000003-2',
-		'ITEM000005',
-		'ITEM000005',
-		'ITEM000005'
+		"ITEM000001",
+		"ITEM000001",
+		"ITEM000001",
+		"ITEM000001",
+		"ITEM000001",
+		"ITEM000003-2",
+		"ITEM000005",
+		"ITEM000005",
+		"ITEM000005"
 	  ];
 	  let expected = [
 		{
-			'barcode':'ITEM000001',
-			'name': '雪碧',
-			'unit': '瓶',
-			'price': 3.00,
-			'count': 5,
-			},
-			{
-			'barcode':'ITEM000003',
-			'name': '荔枝',
-			'unit': '斤',
-			'price': 15.0,
-			'count': 2,
-			},
-			{
-			'barcode':'ITEM000005',
-			'name': '方便面',
-			'unit': '袋',
-			'price': 4.50,
-			'count': 3,
-			},
+			"barcode":"ITEM000001",
+			"name": "雪碧",
+			"unit": "瓶",
+			"price": 3.00,
+			"count": 5,
+		},
+		{
+			"barcode":"ITEM000003",
+			"name": "荔枝",
+			"unit": "斤",
+			"price": 15.0,
+			"count": 2,
+		},
+		{
+			"barcode":"ITEM000005",
+			"name": "方便面",
+			"unit": "袋",
+			"price": 4.50,
+			"count": 3,
+		},
 	  ];
 	expect(printReceipt.constItemDict(input,printReceipt.loadAllItems())).toEqual(expected);
 });
-	it ("constItemDict", () => {
+it ("constItemDict", () => {
 
 	let input = [
 		{"barcode": "ITEM000001", "count": 5, "name": "雪碧", "price": 3, "unit": "瓶"},
 		{"barcode": "ITEM000003", "count": 2, "name": "荔枝", "price": 15, "unit": "斤"},
 		{"barcode": "ITEM000005", "count": 3, "name": "方便面", "price": 4.5, "unit": "袋"}
-	]
+	];
 
 	let expected = [
 		{"barcode": "ITEM000001", "count": 5, "name": "雪碧", "price": 3, "unit": "瓶", "subTotal": 12},
 		{"barcode": "ITEM000003", "count": 2, "name": "荔枝", "price": 15, "unit": "斤", "subTotal": 30},
 		{"barcode": "ITEM000005", "count": 3, "name": "方便面", "price": 4.5, "unit": "袋", "subTotal": 9}
-	]
+	];
 
 	expect(printReceipt.calProm(input,printReceipt.loadPromotions())).toEqual(expected);
 });
@@ -57,16 +57,16 @@ it ("constItemDict", () => {
 it ("printReceipt", () => {
 
 	let input = [
-		'ITEM000001',
-		'ITEM000001',
-		'ITEM000001',
-		'ITEM000001',
-		'ITEM000001',
-		'ITEM000003-2',
-		'ITEM000005',
-		'ITEM000005',
-		'ITEM000005'
-	]
+		"ITEM000001",
+		"ITEM000001",
+		"ITEM000001",
+		"ITEM000001",
+		"ITEM000001",
+		"ITEM000003-2",
+		"ITEM000005",
+		"ITEM000005",
+		"ITEM000005"
+	];
 
 	let expected = "***<store earning no money>Receipt ***\n" 
 		+ "Name: 雪碧, Quantity: 5 瓶, Unit price: 3.00 (yuan), Subtotal: 12.00 (yuan)\n"
@@ -75,37 +75,37 @@ it ("printReceipt", () => {
 		+ "----------------------\n"
 		+ "Total: 51.00 (yuan)\n"
 		+ "Saving: 7.50 (yuan)\n"
-		+ "**********************\n"
+		+ "**********************\n";
 
 	expect(printReceipt.printReceipt(input,printReceipt.loadPromotions())).toEqual(expected);
 });
 
 
-	it('should print text', () => {
+it("should print text", () => {
   
 	  const tags = [
-		'ITEM000001',
-		'ITEM000001',
-		'ITEM000001',
-		'ITEM000001',
-		'ITEM000001',
-		'ITEM000003-2.5',
-		'ITEM000005',
-		'ITEM000005-2',
+		"ITEM000001",
+		"ITEM000001",
+		"ITEM000001",
+		"ITEM000001",
+		"ITEM000001",
+		"ITEM000003-2.5",
+		"ITEM000005",
+		"ITEM000005-2",
 	  ];
   
-	  spyOn(console, 'log');
+	  spyOn(console, "log");
   
 	  console.log(printReceipt.printReceipt(tags,printReceipt.loadPromotions()));
   
-	  const expectText = '***<store earning no money>Receipt ***\n' +
-  'Name: 雪碧, Quantity: 5 瓶, Unit price: 3.00 (yuan), Subtotal: 12.00 (yuan)\n' +
-  'Name: 荔枝, Quantity: 2.5 斤, Unit price: 15.00 (yuan), Subtotal: 37.50 (yuan)\n' +
-  'Name: 方便面, Quantity: 3 袋, Unit price: 4.50 (yuan), Subtotal: 9.00 (yuan)\n' +
-  '----------------------\n' +
-  'Total: 58.50 (yuan)\n' +
-  'Saving: 7.50 (yuan)\n' +
-  '**********************\n';
+	  const expectText = "***<store earning no money>Receipt ***\n" +
+  "Name: 雪碧, Quantity: 5 瓶, Unit price: 3.00 (yuan), Subtotal: 12.00 (yuan)\n" +
+  "Name: 荔枝, Quantity: 2.5 斤, Unit price: 15.00 (yuan), Subtotal: 37.50 (yuan)\n" +
+  "Name: 方便面, Quantity: 3 袋, Unit price: 4.50 (yuan), Subtotal: 9.00 (yuan)\n" +
+  "----------------------\n" +
+  "Total: 58.50 (yuan)\n" +
+  "Saving: 7.50 (yuan)\n" +
+  "**********************\n";
   
 	  expect(console.log).toHaveBeenCalledWith(expectText);
-	});
+});
--- main.js
+++ main.js
@@ -1,115 +1,117 @@
-'use strict';
+"use strict";
 
 function loadAllItems() {
-  return [
-    {
-      barcode: 'ITEM000000',
-      name: '可口可乐',
-      unit: '瓶',
-      price: 3.00
-    },
-    {
-      barcode: 'ITEM000001',
-      name: '雪碧',
-      unit: '瓶',
-      price: 3.00
-    },
-    {
-      barcode: 'ITEM000002',
-      name: '苹果',
-      unit: '斤',
-      price: 5.50
-    },
-    {
-      barcode: 'ITEM000003',
-      name: '荔枝',
-      unit: '斤',
-      price: 15.00
-    },
-    {
-      barcode: 'ITEM000004',
-      name: '电池',
-      unit: '个',
-      price: 2.00
-    },
-    {
-      barcode: 'ITEM000005',
-      name: '方便面',
-      unit: '袋',
-      price: 4.50
-    }
-  ];
+	return [
+		{
+			barcode: "ITEM000000",
+			name: "可口可乐",
+			unit: "瓶",
+			price: 3.00
+		},
+		{
+			barcode: "ITEM000001",
+			name: "雪碧",
+			unit: "瓶",
+			price: 3.00
+		},
+		{
+			barcode: "ITEM000002",
+			name: "苹果",
+			unit: "斤",
+			price: 5.50
+		},
+		{
+			barcode: "ITEM000003",
+			name: "荔枝",
+			unit: "斤",
+			price: 15.00
+		},
+		{
+			barcode: "ITEM000004",
+			name: "电池",
+			unit: "个",
+			price: 2.00
+		},
+		{
+			barcode: "ITEM000005",
+			name: "方便面",
+			unit: "袋",
+			price: 4.50
+		}
+	];
 }
 
 function loadPromotions() {
-  return [
-    {
-      type: 'BUY_TWO_GET_ONE_FREE',
-      barcodes: [
-        'ITEM000000',
-        'ITEM000001',
-        'ITEM000005'
-      ]
-    }
-  ];
+	return [
+		{
+			type: "BUY_TWO_GET_ONE_FREE",
+			barcodes: [
+				"ITEM000000",
+				"ITEM000001",
+				"ITEM000005"
+			]
+		}
+	];
 }
 
 function constItemDict(itemList, allItemList){
 
 	let onlyUnique = function (value, index, self) { 
 		return self.indexOf(value) === index;
-	}
+	};
 
-	let consolidatedItemDict = []
+	let consolidatedItemDict = [];
 	let uniqueItemList = itemList.map((x) => x.split("-")[0]).filter(onlyUnique);
 
 	uniqueItemList.forEach(element => {
-		let newItem = {}
-		
-		let matchedItem = itemList.filter((x)=>((x.substring(0, 10)) === element));
-		let count = 0
-		matchedItem.forEach((item) => {
-			count += (item.includes("-")) ? parseFloat(item.split("-")[1]) : 1
-		})
-		newItem.count = count;
-		newItem.barcode = allItemList.find((x) => x.barcode.substring(0, 10) === element).barcode
-		newItem.name = allItemList.find((x) => x.barcode.substring(0, 10) === element).name
-		newItem.unit = allItemList.find((x) => x.barcode.substring(0, 10) === element).unit
-		newItem.price = allItemList.find((x) => x.barcode.substring(0, 10) === element).price
-		consolidatedItemDict.push(newItem)
+		constructUniqueItem(itemList, element, allItemList, consolidatedItemDict);
 	});
 
 	return consolidatedItemDict;
 }
 
+function constructUniqueItem(itemList, element, allItemList, consolidatedItemDict) {
+	let newItem = {};
+	let matchedItem = itemList.filter((x) => ((x.substring(0, 10)) === element));
+	let count = 0;
+	matchedItem.forEach((item) => {
+		count += (item.includes("-")) ? parseFloat(item.split("-")[1]) : 1;
+	});
+	newItem.count = count;
+	newItem.barcode = allItemList.find((x) => x.barcode.substring(0, 10) === element).barcode;
+	newItem.name = allItemList.find((x) => x.barcode.substring(0, 10) === element).name;
+	newItem.unit = allItemList.find((x) => x.barcode.substring(0, 10) === element).unit;
+	newItem.price = allItemList.find((x) => x.barcode.substring(0, 10) === element).price;
+	consolidatedItemDict.push(newItem);
+}
+
 function calProm(consolidatedItemDict, promList){
-	let consolidatedItemWithPromDict = []
+	let consolidatedItemWithPromDict = [];
 	
 	consolidatedItemDict.forEach((item) => {
-		let newItem = item
-		let buy2Get1Free = promList.find((x)=>x.type === 'BUY_TWO_GET_ONE_FREE')
-		newItem.subTotal = newItem.price * (newItem.count - ((buy2Get1Free.barcodes.includes(item.barcode)) ? parseInt(newItem.count / 3) : 0) )
-		consolidatedItemWithPromDict.push(newItem)
-	})
+		constructItem(item, promList, consolidatedItemWithPromDict);
+	});
 	
-	return consolidatedItemWithPromDict
+	return consolidatedItemWithPromDict;
+}
+
+function constructItem(item, promList, consolidatedItemWithPromDict) {
+	let newItem = item;
+	let buy2Get1Free = promList.find((x) => x.type === "BUY_TWO_GET_ONE_FREE");
+	newItem.subTotal = newItem.price * (newItem.count - ((buy2Get1Free.barcodes.includes(item.barcode)) ? parseInt(newItem.count / 3) : 0));
+	consolidatedItemWithPromDict.push(newItem);
 }
 
 function printReceipt (itemList) {
 
-	let consolidatedItemWithPromDict = calProm(constItemDict(itemList, loadAllItems()), loadPromotions())
-	let result = `***<store earning no money>Receipt ***\n`
-	let total = 0
-	let totalSaved = 0
+	let consolidatedItemWithPromDict = calProm(constItemDict(itemList, loadAllItems()), loadPromotions());
+	let result = "***<store earning no money>Receipt ***\n";
+	let total = 0;
+	let totalSaved = 0;
 	consolidatedItemWithPromDict.forEach((item) => {
-		result += `Name: ${item.name}, Quantity: ${item.count} ${item.unit}, Unit price: ${item.price.toFixed(2)} (yuan), Subtotal: ${item.subTotal.toFixed(2)} (yuan)\n`
-		total += item.subTotal
-		totalSaved += item.price * item.count - item.subTotal 
-	})
-	result += `----------------------\n`
-	result += `Total: ${total.toFixed(2)} (yuan)\n`
-	result += `Saving: ${totalSaved.toFixed(2)} (yuan)\n`
-	result += `**********************\n`
+		({ result, total, totalSaved } = calculateAndPrintItem(result, item, total, totalSaved)); 
+	});
+	result = printFooter(result, total, totalSaved);
 
 	return result;
 }
@@ -121,3 +123,24 @@ module.exports = {
 	loadPromotions,
 	printReceipt
 };
+
+function printFooter(result, total, totalSaved) {
+	result += "----------------------\n";
+	result += `Total: ${total.toFixed(2)} (yuan)\n`;
+	result += `Saving: ${totalSaved.toFixed(2)} (yuan)\n`;
+	result += "**********************\n";
+	return result;
+}
+
+function calculateAndPrintItem(result, item, total, totalSaved) {
+	result = printBody(result, item);
+	total += item.subTotal;
+	totalSaved += item.price * item.count - item.subTotal;
+	return { result, total, totalSaved };
+}
+
+function printBody(result, item) {
+	result += `Name: ${item.name}, Quantity: ${item.count} ${item.unit}, Unit price: ${item.price.toFixed(2)} (yuan), Subtotal: ${item.subTotal.toFixed(2)} (yuan)\n`;
+	return result;
+}
+
